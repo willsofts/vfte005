265d566e26c4c71b1b8a4ece7bc991871f9e37be
tassun <32536248+tassun@users.noreply.github.com>
2024-10-16 12:39:47 +0700

    try to disable button when action

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -134,8 +134,8 @@
         </div>
     </template>
     <template v-slot:footer>
-      <button class="btn btn-dark btn-sm" @click="saveClick" v-if="insertMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.save_button }}</button>
-      <button class="btn btn-dark btn-sm" @click="updateClick" v-if="updateMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.update_button }}</button>
+      <button ref="savebutton" id="savebutton" class="btn btn-dark btn-sm" @click="saveClick" v-if="insertMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.save_button }}</button>
+      <button ref="updatebutton" id="updatebutton" class="btn btn-dark btn-sm" @click="updateClick" v-if="updateMode"><em class="fa fa-save fa-btn-icon"></em>{{ labels.update_button }}</button>
       <button class="btn btn-dark btn-sm" data-dismiss="modal"><em class="fa fa-close fa-btn-icon"></em>{{ labels.cancel_button }}</button>
     </template>
   </DialogForm>
@@ -148,7 +148,7 @@ import { ref, computed, watch } from 'vue';
 import { useVuelidate } from '@vuelidate/core';
 import { required, helpers, email } from '@vuelidate/validators';
 import $ from "jquery";
-import { createNewPassword, DEFAULT_CONTENT_TYPE, getApiUrl }  from '@willsofts/will-app';
+import { createNewPassword, DEFAULT_CONTENT_TYPE, getApiUrl, disableControls }  from '@willsofts/will-app';
 import { startWaiting, stopWaiting, submitFailure, detectErrorResponse }  from '@willsofts/will-app';
 import { confirmUpdate, confirmSave, confirmDelete, successbox, serializeParameters } from '@willsofts/will-app';
 import { InputMask } from '@willsofts/will-control';
@@ -236,12 +236,14 @@ export default {
     },
     async saveClick() {
       console.log("click: save");
+      disableControls($("#savebutton"));
       let valid = await this.validateForm();
       if(!valid) return;
       this.startSaveRecord();
     },
     async updateClick() {
       console.log("click: update");
+      disableControls($("#updatebutton"));
       let valid = await this.validateForm();
       if(!valid) return;
       this.startUpdateRecord();
